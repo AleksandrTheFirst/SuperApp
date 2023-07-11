c8fad9ca158acb956ac7417fa3357bce2d96e400
Aleksandr Zakharov <alexz198383@yandex.com>
2023-07-11 17:32:57 +0300

    Забыл конструкторы и свойство name.

--- CoreJavaHomeWorks/src/main/java/InterfaceHw/Car.java
+++ CoreJavaHomeWorks/src/main/java/InterfaceHw/Car.java
@@ -1,6 +1,9 @@
 package InterfaceHw;
 
 public class Car implements ICar{
+    private String name;
+
+
     @Override
     public void drive() {
         System.out.println("Tesla drive");
@@ -9,5 +12,20 @@ public class Car implements ICar{
     @Override
         public void stop() {
         System.out.println("Tesla stop");
+    }
+
+    public Car() {
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
